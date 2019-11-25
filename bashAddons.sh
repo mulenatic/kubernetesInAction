30137d9537ce1af41cf4914e0a6594bca0b7b96c
user <user>
2019-11-25 07:23:41 +0000

    Added a alias to quickly change the k8s namespace via bash.

--- bashAddons.sh
+++ bashAddons.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Kubernetes
 alias k=kubectl
 source <(kubectl completion bash | sed s/kubectl/k/g)
+alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
 
+# Emacs Docker
 alias emacs-docker='docker run -it --rm -v "$(pwd)":/emacsDocuments mulenatic/emacs-docker'
+
