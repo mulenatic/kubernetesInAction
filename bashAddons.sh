9a3cf3ab13cc5365c86192d0f3a87f6037229842
user <user>
2019-11-26 10:11:44 +0000

    Added new bash alias.

--- bashAddons.sh
+++ bashAddons.sh
@@ -5,6 +5,9 @@ alias k=kubectl
 source <(kubectl completion bash | sed s/kubectl/k/g)
 alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
 
+# GCE
+alias gceProj='gcloud config set project core-gearbox-259809'
+
 # Emacs Docker
 alias emacs-docker='docker run -it --rm -v "$(pwd)":/emacsDocuments mulenatic/emacs-docker'
 
