d9f2c3fd9a9e5e20e9a146d40b2dec733d6d77a8
user <user>
2019-12-02 07:56:50 +0000

    Added an alias to create a google cluster with gcloud.

--- bashAddons.sh
+++ bashAddons.sh
@@ -4,6 +4,7 @@
 alias k=kubectl
 source <(kubectl completion bash | sed s/kubectl/k/g)
 alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
+alias createGoogleCluster='gcloud container clusters create kubia --num-nodes 3 --machine-type "f1-micro" --zone "europe-west1-c"'
 
 # GCE
 alias gceProj='gcloud config set project core-gearbox-259809'
