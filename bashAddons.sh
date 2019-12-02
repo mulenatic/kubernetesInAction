c727d777e6a8214afe22237149ccf9b8b4200327
user <user>
2019-12-02 09:06:17 +0000

    The cluster needs better hardware.

--- bashAddons.sh
+++ bashAddons.sh
@@ -4,7 +4,7 @@
 alias k=kubectl
 source <(kubectl completion bash | sed s/kubectl/k/g)
 alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
-alias createGoogleCluster='gcloud container clusters create kubia --num-nodes 3 --machine-type "f1-micro" --zone "europe-west1-c"'
+alias createGoogleCluster='gcloud container clusters create kubia --num-nodes 3 --machine-type "g1-small" --zone "europe-west1-b"'
 
 # GCE
 alias gceProj='gcloud config set project core-gearbox-259809'
