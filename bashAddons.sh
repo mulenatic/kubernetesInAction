7201944f7d560f4c491a0b7253dace4ae69240ec
user <user>
2019-12-02 13:56:36 +0000

    Added alias to create a micro cluster.apiVersion: extensions/v1beta1 kind: Deployment metadata: name: prod spec: replicas: 5 template: spec: ... tolerations: - key: node-type Operator: Equal value: production effect: NoScheduleapiVersion: extensions/v1beta1 kind: Deployment metadata: name: prod spec: replicas: 5 template: spec: ... tolerations: - key: node-type Operator: Equal value: production effect: NoSchedule

--- bashAddons.sh
+++ bashAddons.sh
@@ -5,6 +5,7 @@ alias k=kubectl
 source <(kubectl completion bash | sed s/kubectl/k/g)
 alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
 alias createGoogleCluster='gcloud container clusters create kubia --num-nodes 3 --machine-type "g1-small" --zone "europe-west1-b"'
+alias createGoogleClusterMicro='gcloud container clusters create kubia-micro --num-nodes 3 --machine-type "f1-micro" --zone "europe-west1-c"'
 
 # GCE
 alias gceProj='gcloud config set project core-gearbox-259809'
